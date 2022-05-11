4e3ac4a970d1d7d41eeaa3a50eafab01ebf0a489
Sachin Chaudhary <sachinchaudhary8765@gmail.com>
2022-05-11 19:06:00 +0530

    Setup app for Heroku

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude , longitude , callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=cbee62239c70f18146c1488a3ce18c37&query=${latitude},${longitude}&units=f`
+    const url = `http://api.weatherstack.com/current?access_key=cbee62239c70f18146c1488a3ce18c37&query=${latitude},${longitude}&units=m`
     request({url , json: true} , (error , { body }) => {
         if(error) {
             callback('Unable to connect to weather service!' , undefined)
@@ -10,7 +10,7 @@ const forecast = (latitude , longitude , callback) => {
             callback('Unable to find location' , undefined)
         }
         else {
-            callback(undefined , `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`)
+            callback(undefined , `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees and it feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}%`)
         }
     })
 }
